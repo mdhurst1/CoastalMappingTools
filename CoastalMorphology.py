189316bc6415336a327be2b57dec8417f204e6ff
Martin Hurst <Martin.Hurst@glasgow.ac.uk>
2019-02-17 14:00:10 +0000

    Adding swath profiler tool

--- CoastalMorphology.py
+++ CoastalMorphology.py
@@ -9,7 +9,8 @@ Febraury 2019
 
 # import modules
 import numpy as np
-from scipy.signal import savgol_filter, medfilt
+from scipy.signal import savgol_filter
+import rasterio
 import shapefile
 
 def ReadCoastlineShp(CoastLineShp):
@@ -318,20 +319,197 @@ def GenerateCoastalNormals(CoastLineShp,ProfSpacing,l2sea = 1500.0,l2land = 200.
     f = open(CoastTransectsShp.rstrip("shp")+"prj","w")
     f.write(Projection)
     f.close()
+
+def ExtractSwathProfiles(CoastTransectsShp,DTM,SwathDist):
     
+    """
+    
+    Function to extract swath profile elevation data for each coastal transect
+    
+    Martin Hurst, February 2019
+    University of Glasgow
+    
+    """
+    
+    print("ExtractSwathProfiles: Generating Elevation Swath Profiles perpendicular to the coast")
+
+    # load the DTM
+    DTM_Dataset = rasterio.open(DTM)
+    DTMArray = DTM_Dataset.read(1)
+    NCols = DTM_Dataset.height
+    NRows = DTM_Dataset.width
+    NDV = DTM_Dataset.nodata
+    Resolution = DTM_Dataset.res
+    
+    print(NCols,NRows,NDV,Resolution)
+    
+#    DEM_Resolution = 5.
+#    PointSpacing = DEM_Resolution/2.
+#    
+#    # Set up workspace
+#    JunkFolder = Path + "Junk\\"
+#    ProfsFolder = Path + "Profs\\"
+#
+#    if os.path.exists(JunkFolder) is False:
+#        os.mkdir(JunkFolder)
+#
+#    if os.path.exists(ProfsFolder) is False:
+#        os.mkdir(ProfsFolder)
+#
+#    #Declare variables
+#    DEM = Path+DEM
+#    #DEM = "S:/NextMap/merged/dtm/" + DEM.lower()
+#    ProfLinesShp = ProfsFolder+"profile_lines.shp"
+#
+#    # Open coast polyline file for reading
+#    sf = shp.Reader(ProfLinesShp)
+#
+#    # Get line segment shapes
+#    Shapes = sf.shapes()
+#    NoLines = len(Shapes)
+#
+#    #Get DEM properties
+#    Description = arcpy.Describe(DEM)
+#    Extent = Description.Extent
+#    DataResolution = Description.meanCellHeight
+#    XMin = Extent.XMin
+#    YMin = Extent.YMin
+#    YMax = Extent.YMax
+#    XMax = Extent.XMax
+#    
+#    NCols = int(Extent.width/DataResolution)
+#    NRows = int(Extent.height/DataResolution)
+#
+#    #Convert DEM to numpy array
+#    #Downloaded ArcGIS 10.1 SP1 for Desktop Background Geoprocessing (64-bit) from
+#    #http://support.esri.com/en/downloads/patches-servicepacks/view/productid/189/metaid/1913
+#    DEMArray = arcpy.RasterToNumPyArray(DEM)
+#    XVector = XMin+np.arange(0,NCols)*DataResolution
+#    YVector = YMin+DataResolution*np.arange(0,NRows)[::-1]
+#
+#    #loop through lines and get Swath Profile Elevations
+#    for l in range(0,NoLines):
+#
+#        #Get line points
+#        X1,Y1 = Shapes[l].points[0]
+#        X2,Y2 = Shapes[l].points[1]
+#
+#        #find indices for bounding box
+#        #need to be careful with reverse indexing
+#        iStart = np.argmin(np.abs(YVector-np.max([Y1,Y2])))-1
+#        iEnd = np.argmin(np.abs(YVector-np.min([Y1,Y2])))+1
+#        jStart = np.argmin(np.abs(XVector-np.min([X1,X2])))-1
+#        jEnd = np.argmin(np.abs(XVector-np.max([X1,X2])))+1
+#
+#        #Get Vector X and Y
+#        dX12 = X2-X1
+#        dY12 = Y2-Y1
+#
+#        #Declare list holders for profile data
+#        X = []
+#        Y = []
+#        Z = []
+#        DistAlong = []
+#        DistTo = []
+#        
+#        f = open(ProfsFolder+"Prof" + str(l) + ".txt","w")
+#        f.write("X Y DistAlong DistTo Z\n")
+#        
+#        for i in range(iStart,iEnd):
+#
+#            #get Y position
+#            YNode = YMax-DataResolution*(i)
+#
+#            for j in range(jStart,jEnd):
+#                
+#                #get X position
+#                XNode = XMin + j*DataResolution;
+#
+#                #Get 2nd Vector Properties in Array
+#                dX13 = XNode-X1
+#                dY13 = YNode-Y1
+#
+#                #Find Dot Product
+#                DotProduct = dX12*dX13 + dY12*dY13;
+#
+#                #calculate fraction of distance along line
+#                t = DotProduct/(dX12*dX12 + dY12*dY12)
+#                if ((t < 0.) or (t > 1.)):
+#                    continue
+#
+#                #Find point along line
+#                XLine = X1 + t*dX12
+#                YLine = Y1 + t*dY12
+#                DistanceAlongLine = t*np.sqrt(dX12*dX12 + dY12*dY12)
+#
+#                #find distance to point
+#                DistanceToLine = np.sqrt((XLine-XNode)*(XLine-XNode) + (YLine-YNode)*(YLine-YNode))
+#
+#                if (DistanceToLine < SwathDist):
+#                    X.append(XNode)
+#                    Y.append(YNode)
+#                    DistAlong.append(DistanceAlongLine)
+#                    DistTo.append(DistanceToLine)
+#                    Z.append(DEMArray[i][j])
+#                        
+#        #Sort by distance along line
+#        Sortedi = np.argsort(DistAlong)
+#        X = X[Sortedi]
+#        Y = Y[Sortedi]
+#        DistAlong = DistAlong[Sortedi]
+#        DistTo = DistTo[Sortedi]
+#        Z = Z[Sortedi]
+#        
+#        #Create a line for interpolating to
+#        LineLength = np.sqrt((X2-X1)**2 + (Y2-Y1)**2)
+#        NoPoints = LineLength/PointSpacing
+#        XLine = np.linspace(X1,X2,NoPoints)
+#        YLine = np.linspace(Y1,Y2,NoPoints)
+#        ZLine = np.zeros(len(XLine))
+#        DistAlongLine = np.zeros(len(XLine))
+#        
+#        #Loop along line
+#        for i in range(0,NoPoints):
+#            
+#            #Calculate distance along the line
+#            DistAlongLine[i] = i*PointSpacing
+#            
+#            #Could sample a reduced array here i.e. a neighbourhood?
+#            Neighbourhood = np.abs(DistAlongLine-DistAlong) < SwathDist
+#            
+#            #Create a distance vector
+#            Dist = np.sqrt(DistAlong[Neighbourhood]**2. + DistTo[Neighbourhood]**2.)
+#            
+#            #Weights are inverse
+#            Weights = 1./Dist**2.
+#            
+#            #Interpolate Z
+#            ZLine[i]  = np.sum(Z[Neighbourhood]*Weights)/np.sum(Weights)
+#        
+#        #Save IDW profile to a file
+#        f.write(str(XNode)+" "+str(YNode)+" "+str(DistanceAlongLine)+" "+str(DistanceToLine)+" "+str(DEMArray[i][j])+"\n")
+#                        
+#        #close file
+#        f.close()
+        
 if __name__ == "__main__":
     
     # declare file names
     CoastLineShp = "D:/NCCA2/StAndrews/MHWS/MHWS_2018_Dissolve.shp"
-    #MergedCoastLineShp = "D:/NCCA2/StAndrews/MHWS/MHWS_2018_Merged.shp"
+    MergedCoastLineShp = "D:/NCCA2/StAndrews/MHWS/MHWS_2018_Merged.shp"
     SmoothCoastLineShp = "D:/NCCA2/StAndrews/MHWS/MHWS_2018_Smooth.shp"
+    CoastTransectsShp = "D:/NCCA2/StAndrews/MHWS/MHWS_2018_Smooth_transects.shp"
+    DTM = "D:/NCCA2/StAndrews/DTM/StAn_2018_DTM.tif"
     
     # launch merging
     #MergeCoastline(CoastLineShp, MergedCoastLineShp)
     
     # launch smoothing
-    SmoothCoastline(CoastLineShp,SmoothCoastLineShp)
+    #SmoothCoastline(CoastLineShp,SmoothCoastLineShp)
     
     # generate normals
-    GenerateCoastalNormals(SmoothCoastLineShp,50.,200.,50.)
-     
+    #GenerateCoastalNormals(SmoothCoastLineShp,50.,200.,50.)
+    
+    # extract swath profiles
+    ExtractSwathProfiles(CoastTransectsShp,DTM,SwathDist)
+        
---
+++ Transect.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 17 12:23:28 2019
+
+@author: mh322u
+"""
+
+class Transect():
+    
