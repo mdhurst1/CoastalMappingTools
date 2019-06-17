39a6044ba4393b12e67ce2e33774e470aaf02a59
Martin Hurst <Martin.Hurst@glasgow.ac.uk>
2019-06-17 17:17:29 +0100

    Some more updates to geometry objects

--- Coast.py
+++ Coast.py
@@ -19,7 +19,13 @@ class Coast:
     """
 
     def __init__(self,CoastShp=""):
+        """
+        MDH, June 2019
+        """
         self.CoastShp = CoastShp
+        self.Shapes = []
+        self.Fields = []
+        self.Records = []
         self.NoCoastLines = 0
         self.CoastLines = []
         self.Projection = ""
@@ -36,9 +42,9 @@ class Coast:
 
         # Open coast polyline file for reading
         SF = shapefile.Reader(CoastShp)
-        Shapes = SF.shapes()
-        Fields = SF.fields
-        Records = SF.records()
+        self.Shapes = SF.shapes()
+        self.Fields = SF.fields
+        self.Records = SF.records()
 
         # Get number of coast segments to work on
         self.NoCoastLines = len(Shapes)
@@ -63,20 +69,79 @@ class Coast:
         
         # reduce to single smoothed coastline
     
-    return Shapes, Fields, Records, NoShapes, Projection 
 
 
-    def SmoothCoastlines(self):
+    def MergeCoastlines(self):
+        """
+        Identifies individual coast Lines that are touching at one end 
+        and combines them into a single Line
+
+        MDH, June 2019
+        """
+
+        print("Coast: Merging coastlines")
+
+        for i in range(0, self.NoCoastLines):
+                            
+            # get X and Y coordinates of both Lines
+            # segment 1 only needs defining first time round as will be dynamic
+            if i == 0:
+                X1, Y1 = self.Lines[i].get_XY()
+        
+            # only define second segment and test if not at the end of the file
+            if i < self.NoCoastLines-1:
+                X2, Y2 = self.Lines[i+1].get_XY())
+
+            # check for a match
+            if ((X1[-1] == X2[0]) and (Y1[-1] == Y2[0])):
+                X1 = np.concatenate((X1,X2[1:]))
+                Y1 = np.concatenate((Y1,Y2[1:]))
+
+                # write new line and delete second segment
+                self.Lines[i] = Line(X, Y, str(i))
+                Lines.pop(i+1)
+
+                # update shapefile lists and delete second segment (this will need testing)
+                self.Shapes[i] = np.column_stack([X1,Y1]).tolist()
+                self.Shapes.pop(i+1)
+                self.Records.pop(i+1)
+
+
+    def SmoothCoastlines(self, WindowSize):
         """
         Wrapper to the function in the Line object
+
+        Savitzky and Golay (1964) smoothing filter
+    
+        Savitzky, A. and Golay, M. J.: Smoothing and differentiation of data
+        by simplified least squares procedures, Anal. Chem., 36, 1627–
+        1639, 1964.
+
+        MDH, June 2019
         """
 
         print("Coast: Smoothing coastlines")
+
+        for i, Line in enumerate(self.Lines):
+
+            # smooth the line
+            Line.SmoothLine(WindowSize)
+
+            # update the shape object list
+            X, Y = Line.get_XY()
+            self.Shapes[i] = np.column_stack([X,Y]).tolist()
 
 
     # function to do something    
     def GenerateNormals(self):
         """
         Wrapper to the function in the Line object
+
+        Generates transects perpendicular to the coastline
         """
         print("Coast: Generating coastline normals")
+
+        for i, Line in enumerate(self.Lines):
+
+            # smooth the line
+            Line.GenerateTransects()
--- Line.py
+++ Line.py
@@ -10,6 +10,9 @@ June 2019
 # import modules
 import numpy as np
 import shapefile
+from scipy.signal import savgol_filter
+import Transect
+import Nodes
 
 class Line:
     """
@@ -37,3 +40,43 @@ class Line:
         YSmooth = savgol_filter(Y,WindowSize,PolyOrder, mode="nearest")
 
         # Write new X and Y vectors to Nodes
+    
+    def GenerateBuffers(self,Dist1,Dist2):
+        """
+        Description goes here
+
+        MDH, June 2019
+        """
+
+        
+
+    def GenerateTransects(self,Args):
+        """
+        Description goes here
+
+        MDH, June 2019
+        """
+
+        # Get X and Y vectors from Nodes
+
+        # smooth X and Y individually with Savitzky Golay filter
+        # window size and polyorder must be integers you idiot!
+        PolyOrder = 4
+        XSmooth = savgol_filter(X,WindowSize,PolyOrder, mode="nearest")
+        YSmooth = savgol_filter(Y,WindowSize,PolyOrder, mode="nearest")
+
+        # Write new X and Y vectors to Nodes
+
+    def get_XY(self):
+        """
+        Returns X and Y coordinates as vector numpy arrays
+
+        MDH, June 2019
+        """
+        X = []
+        Y = []
+        for i in range(0,NoNodes):
+            X.append(Nodes[i].X)
+            Y.append(Nodes[i].Y)
+        
+        return np.array(X), np.array(Y)
