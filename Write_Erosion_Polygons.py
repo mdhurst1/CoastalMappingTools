ab2456374bd0a26dc99d8e5f9f094a2094d420df
mdhurst1 <martin.d.hurst@gmail.com>
2021-02-25 11:07:03 +0000

    function to write decadal erosion polygons

---
+++ Write_Erosion_Polygons.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 23 15:07:21 2021
+
+@author: mhurst
+"""
+
+# import modules
+import pickle, pathlib, sys
+import pandas as pd
+import geopandas as gp
+from Coast import *
+
+#set up working directory
+WorkingPath = pathlib.Path.cwd().parent
+
+# get all coastal cells to loop through
+Cells = gp.read_file(WorkingPath / "CoastalCells" / "CoastalCells_Partitioned.shp")
+
+# set up scenarios
+Scenarios = [2,4,8]
+Percentiles = [50,50,95]
+Decades = [2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090, 2100]
+
+#loop through scenarios
+for Scenario, Percentile in zip(Scenarios, Percentiles):
+    
+    print("RCP", Scenario, Percentile)
+    
+    # set up output folder
+    OpenPath = WorkingPath/("RCP_"+str(Scenario)+"_"+str(Percentile)+"th_OpenCoast")
+    InnerPath = WorkingPath/("RCP_"+str(Scenario)+"_"+str(Percentile)+"th_InnerCoast")
+        
+    #Loop through Cells
+    for index, Row in Cells.iterrows():
+        CellSub = Row.Cell_sub
+        RowName = "Cell_"+CellSub
+        print("\t", RowName)
+        
+        OpenCoastFlag = False
+        InnerCoastFlag = False
+        
+        # load the coast objects
+        OpenCoastFilename = OpenPath / (RowName+"_Change.pydata")
+        try:
+            OpenCoast = pickle.load( open( OpenCoastFilename, "rb" ) )
+            OpenCoastFlag = True
+            
+        except:
+            print("No Open Coast Object")
+            
+        
+        OpenCoastFilename = OpenPath / (RowName+"_Change.pydata")
+        try:
+            InnerCoast = pickle.load( open( OpenCoastFilename, "rb" ) )
+            InnerCoastFlag = True
+
+        except:
+            print("No Inner Coast Object")
+           
+        OpenCoast.WriteFutureShorelinesShp(str(OpenPath / (RowName + "_Future.shp")), Smooth=True)
+        InnerCoast.WriteFutureShorelinesShp(str(InnerPath / (RowName + "_Future.shp")), Smooth=True)
+        
+        #Loop through decades
+        for i, Decade in enumerate(Decades):
+            
+            print("\t\t", Decade)
+            
+            #skip 2020
+            if i == 0:
+                continue
+            
+            #Write outputs for each decade both inner and open
+            if OpenCoastFlag:
+                
+                OpenCoast.WriteErodedAreaShp(str(OpenPath / (RowName + "_ErodedArea_" + str(Decade) + ".shp")), Year=Decade)
+                OpenCoast.WriteErodedAreaShp(str(OpenPath / (RowName + "_ErodedArea_" + str(Decades[i-1])+"_"+str(Decade) + ".shp")), StartYear = Decades[i-1], Year=Decade)
+            
+            if InnerCoastFlag:
+                
+                InnerCoast.WriteErodedAreaShp(str(InnerPath / (RowName + "_ErodedArea_" + str(Decade) + ".shp")), Year=Decade)
+                InnerCoast.WriteErodedAreaShp(str(InnerPath / (RowName + "_ErodedArea_" + str(Decades[i-1])+"_"+str(Decade) + ".shp")), StartYear = Decades[i-1], Year=Decade)
+            
+
+# merge results
+#loop through scenarios
+for Scenario, Percentile in zip(Scenarios, Percentiles):
+    
+    print(Scenario)
+    
+    # set up output folder
+    OpenPath = WorkingPath/("RCP_"+str(Scenario)+"_"+str(Percentile)+"th_OpenCoast")
+    InnerPath = WorkingPath/("RCP_"+str(Scenario)+"_"+str(Percentile)+"th_InnerCoast")
+    FilePaths = [OpenPath, InnerPath]
+    PrefixNames = ["Open", "Inner"]
+    
+    #Do inner then outer
+    for FilePath, PrefixName in zip(FilePaths,PrefixNames):
+        
+        print(PrefixName)
+        
+        #loop over decades
+        for i, Decade in enumerate(Decades):
+            
+            print(Decade)
+            
+            # skip 2020
+            if i == 0:
+                continue
+            
+            ErodedAreaList = []
+            DecadalAreaList = []
+            
+            # loop through cells
+            for index, Row in Cells.iterrows():
+                CellSub = Row.Cell_sub
+                RowName = "Cell_"+CellSub
+                
+                # load future area file and append
+                FutureFile = FilePath / (RowName + "_ErodedArea_" + str(Decade) + ".shp")
+                
+                if FutureFile.exists():
+                    TempGDF = gp.read_file(FutureFile)
+                    ErodedAreaList.append(TempGDF)
+                    
+                # load future area file and append
+                FutureFile = FilePath / (RowName + "_ErodedArea_" + str(Decades[i-1])+"_"+str(Decade) + ".shp")
+                
+                if FutureFile.exists():
+                    TempGDF = gp.read_file(FutureFile)
+                    DecadalAreaList.append(TempGDF)
+                    
+            # write new file
+            try:
+                WriteGDF = pd.concat(ErodedAreaList, sort=True)
+                WriteGDF.to_file(FilePath / ("Scotland_" + PrefixName + "_ErodedArea_" + str(Decade) + ".shp"))
+            except:
+                import pdb
+                pdb.set_trace()
+                
+            
+            WriteGDF = pd.concat(DecadalAreaList, sort=True)
+            WriteGDF.to_file(FilePath / ("Scotland_" + PrefixName + "_ErodedArea_" + str(Decades[i-1])+"_"+str(Decade) + ".shp"))
