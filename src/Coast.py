c6ba4810be778449d182f4f0833a0375b2d22dc7
Nicola Horsburgh <136325339+nicolaH37@users.noreply.github.com>
2023-10-19 15:15:06 +0100

    ExtractTransectTopographySwath: Extend bounding box

--- src/Coast.py
+++ src/Coast.py
@@ -3346,20 +3346,32 @@ class Coast:
 
                     #find indices for bounding box
                     #need to be careful with reverse indexing
-                    iStart = np.argmin(np.abs(YVector-np.max([Y1,Y2])))-1
-                    iEnd = np.argmin(np.abs(YVector-np.min([Y1,Y2])))+1
-                    jStart = np.argmin(np.abs(XVector-np.min([X1,X2])))-1
-                    jEnd = np.argmin(np.abs(XVector-np.max([X1,X2])))+1
+                    #iStart = np.argmin(np.abs(YVector-np.max([Y1,Y2])))-1
+                    #iEnd = np.argmin(np.abs(YVector-np.min([Y1,Y2])))+1
+                    #jStart = np.argmin(np.abs(XVector-np.min([X1,X2])))-1
+                    #jEnd = np.argmin(np.abs(XVector-np.max([X1,X2])))+1
+                    
+                    # NH: Change bounding box size to extend past transect bounds by SwathDistance
+                    iStart = np.argmin(np.abs(YVector-np.max([Y1,Y2])))-(int)(SwathDistance/DTM_Resolution) 
+                    iEnd = np.argmin(np.abs(YVector-np.min([Y1,Y2])))+(int)(SwathDistance/DTM_Resolution)
+                    jStart = np.argmin(np.abs(XVector-np.min([X1,X2])))-(int)(SwathDistance/DTM_Resolution)
+                    jEnd = np.argmin(np.abs(XVector-np.max([X1,X2])))+(int)(SwathDistance/DTM_Resolution)
                     
                     # NH: Add index sanity check
                     # Catch Start index of -1, in case where transect intersects top (i) or left hand side (j) of DEM.
-                    # Bottom or rhs intersection (e.g. End index of 1000) not a problem, due to the range function going to End-1
+                    # Catch End index larger than the length or width of the DTM. This only applies for wider bounding box.  
                     if iStart < 0:
                         print("\t\t\t\tiStart < 0! Setting to 0")
                         iStart = 0
                     if jStart < 0:
                         print("\t\t\t\tjStart < 0! Setting to 0")
                         jStart = 0
+                    if iEnd > len(YVector):
+                        print("\t\t\t\tiEnd > len(YVector! Setting to", len(YVector))
+                        iEnd = len(YVector)
+                    if jEnd > len(XVector):
+                        print("\t\t\t\tjEnd > len(XVector! Setting to", len(XVector))
+                        jEnd = len(XVector)    
                     
                     if __debug__:
                         print("\t\t\t\tiStart, iEnd, jStart, jEnd = ", iStart, iEnd, jStart, jEnd)
