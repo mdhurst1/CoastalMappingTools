f84a3bc8477d8de1d7f8c5ab32f6f6eb44a84a9c
Nicola Horsburgh <136325339+nicolaH37@users.noreply.github.com>
2023-12-05 13:29:33 +0000

    Add accompanying Transect parameters for setup and swash commponents

--- src/Transect.py
+++ src/Transect.py
@@ -198,6 +198,21 @@ class Transect:
         self.M85_R2 = None
         self.E45_R2 = None
         self.E85_R2 = None
+        self.H_setup = None
+        self.M45_setup = None
+        self.M85_setup = None
+        self.E45_setup = None
+        self.E85_setup = None
+        self.H_swash = None
+        self.M45_swash = None
+        self.M85_swash = None
+        self.E45_swash = None
+        self.E85_swash = None
+        self.H_Dissipative = None
+        self.M45_Dissipative = None
+        self.M85_Dissipative = None
+        self.E45_Dissipative = None
+        self.E85_Dissipative = None
         # extreme still water level
         self.H_ESL = None
         self.H_ESL_c1 = None
