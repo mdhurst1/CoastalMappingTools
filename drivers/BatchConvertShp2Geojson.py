c3779750fb840c68e478eb9e68cb52672b3fe4e3
mdhurst1 <martin.d.hurst@gmail.com>
2026-07-24 10:19:25 +0100

    some useful tools and addtional data proj

--- drivers/BatchConvertShp2Geojson.py
+++ drivers/BatchConvertShp2Geojson.py
@@ -14,20 +14,22 @@ def BatchConvertShp2Geojson(InputFolder, OuputFolder):
     # loop through all shp in folder
     for Shapefile in InputFolder.rglob("*.shp"):
 
+        # print progress to screen
+        print(f"Converting {Shapefile.name} to geojson")
         # rename with new extension
-        Outputfile = OutputFolder / f"{Shapefile.stem}.geojson"
+        OutputFile = OutputFolder / f"{Shapefile.stem}.geojson"
 
         # read shapefile to geodataframe
         GDF = gpd.read_file(Shapefile)
 
-        if gdf.crs is None:
+        if GDF.crs is None:
             print(f"Skipping {Shapefile.name}: no CRS defined")
             continue
 
         GDF = GDF.to_crs("EPSG:4326")
         GDF.to_file(OutputFile, driver="GeoJSON")
 
-if __name__ == "__main__"
+if __name__ == "__main__":
 
     # setup folders
     InputFolder = Path(r"C:\Users\mh322u\OneDrive - University of Glasgow\Python\CoastalMappingTools\example_data\Montrose")
